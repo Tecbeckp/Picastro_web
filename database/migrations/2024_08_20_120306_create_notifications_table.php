e2a765108623daa41aaeefe56a9d5506d5a9447c
danyal hassan <danyalhassan651@gmail.com>
2024-09-11 14:57:18 +0500

    set migration

--- database/migrations/2024_08_20_120306_create_notifications_table.php
+++ database/migrations/2024_08_20_120306_create_notifications_table.php
@@ -16,6 +16,12 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('type')->nullable();
             $table->string('notification')->nullable();
+            $table->integer('follower_id')->nullable();
+            $table->integer('post_image_id')->nullable();
+            $table->integer('trophy_id')->nullable();
+            $table->integer('comment_id')->nullable();
+            $table->integer('reply_comment_id')->nullable();
+            $table->enum('is_read',['0','1'])->default('0');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
