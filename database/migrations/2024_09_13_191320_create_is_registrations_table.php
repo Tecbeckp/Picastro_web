27c0623793bd38506a9cfa970c542f55d963c2a8
danyal hassan <danyalhassan651@gmail.com>
2024-09-13 19:16:56 +0500

    set register api

---
+++ database/migrations/2024_09_13_191320_create_is_registrations_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('is_registrations', function (Blueprint $table) {
+            $table->id();
+            $table->enum('is_registration',['0','1'])->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('is_registrations');
+    }
+};
