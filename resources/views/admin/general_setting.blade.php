7d4e3dac7cab0cc41bfb653388adcd79e1871b5e
danyal hassan <danyalhassan651@gmail.com>
2024-10-16 19:08:03 +0500

    set general page

--- resources/views/admin/general_setting.blade.php
+++ resources/views/admin/general_setting.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'General Setting | Picastro')
+@section('title', 'General Settings | Picastro')
 @section('content')
     <div class="page-content">
         <div class="container-fluid">
@@ -8,12 +8,12 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between bg-galaxy-transparent">
-                        <h4 class="mb-sm-0">General Setting</h4>
+                        <h4 class="mb-sm-0">General Settings</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="javascript: void(0);">Content Page</a></li>
-                                <li class="breadcrumb-item active">General Setting</li>
+                                <li class="breadcrumb-item active">General Settings</li>
                             </ol>
                         </div>
 
@@ -26,7 +26,7 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header align-items-center d-flex">
-                            <h4 class="card-title mb-0">General Setting</h4>
+                            <h4 class="card-title mb-0">General Settings</h4>
                         </div><!-- end card header -->
 
                         <div class="card-body">
