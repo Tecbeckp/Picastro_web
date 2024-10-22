b95adeeec5272b1e4db3bea084b678b4cf8006bd
danyal hassan <danyalhassan651@gmail.com>
2024-10-22 19:13:48 +0500

    set image quality

--- app/Traits/UploadImageTrait.php
+++ app/Traits/UploadImageTrait.php
@@ -20,7 +20,7 @@ trait  UploadImageTrait
 
         $image = Image::read($file)
                       ->resize()
-                      ->save($localFilePath, 75);
+                      ->save($localFilePath, 30);
     
         Storage::disk('s3')->put($destinationFolder . time() . '-' . $fileName, file_get_contents($localFilePath));
     
