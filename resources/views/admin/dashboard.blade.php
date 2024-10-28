8c0c6abf2b6ff0f8f256da689ec68b0c93f7e668
danyal hassan <danyalhassan651@gmail.com>
2024-10-28 11:07:53 +0500

    set dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -216,7 +216,7 @@
                                             <th scope="col">Name</th>
                                             <th scope="col">Email</th>
                                             <th scope="col">Username</th>
-                                            <th scope="col">Status</th>
+                                            <th scope="col">Subscription Status</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -238,7 +238,7 @@
                                                     {{ $user->username ?? 'N/A' }}
                                                 </td>
                                                 <td>
-                                                    @if ($user->status == '1')
+                                                    @if ($user->subscription == '1')
                                                         <span
                                                             class="badge bg-success-subtle text-success text-uppercase">Active</span>
                                                     @else
