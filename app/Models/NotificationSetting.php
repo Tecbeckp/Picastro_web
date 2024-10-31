2c68489ff185ed2446ae6881e33057b30a0b7bcd
danyal hassan <danyalhassan651@gmail.com>
2024-10-31 13:07:56 +0500

    set

--- app/Models/NotificationSetting.php
+++ app/Models/NotificationSetting.php
@@ -10,4 +10,67 @@ class NotificationSetting extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function getFollowAttribute($value){
+        if($value == '1'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getPostAttribute($value){
+        if($value == '1'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getTrophyAttribute($value){
+        if($value == '1'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getStarAttribute($value){
+        if($value == '1'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getCommentAttribute($value){
+        if($value == '1'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getCommentReplyAttribute($value){
+        if($value == '1'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getLikeCommentAttribute($value){
+        if($value == '1'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function getOtherAttribute($value){
+        if($value == '1'){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
