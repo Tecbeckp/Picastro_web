71dfd00b6023c370d73d5853ebc26dcf98decd99
danyal hassan <danyalhassan651@gmail.com>
2024-11-12 11:13:00 +0500

    set api

--- routes/api.php
+++ routes/api.php
@@ -46,7 +46,7 @@ Route::get('/create-plan', [PaymentController::class, 'createPlan']);
 Route::get('get-all-test-post-image', [PostImageController::class, 'allTestPostImage']);
 Route::get('/pusher-common-keys', [ApiGeneralController::class ,'pusherCommonKeys']);
 Route::post('send-gift', [ApiGeneralController::class, 'sendGift'])->name('sendGift');
-
+Route::get('apply-coupon', [ApiGeneralController::class, 'applyCoupon'])->name('applyCoupon');
 Route::group(['middleware' => 'auth:sanctum'], function () {    
     Route::post('pusher-auths', [ApiGeneralController::class, 'pusherAuths']); 
     
@@ -106,7 +106,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('get-following', [ApiGeneralController::class, 'getFollowing'])->name('getFollowing');
     Route::get('get-given-star-users', [ApiGeneralController::class, 'getGivenStarUser'])->name('getGivenStarUser');
     Route::get('all-over-search', [ApiGeneralController::class, 'AllOverSearch'])->name('AllOverSearch');
-    Route::get('apply-coupon', [ApiGeneralController::class, 'applyCoupon'])->name('applyCoupon');
     Route::get('hide-post', [ApiGeneralController::class, 'HidePost'])->name('HidePost');
     Route::get('image-of-week', [ApiGeneralController::class, 'imageOfweek']);
     Route::post('update-notification-setting', [ApiGeneralController::class, 'UpdateNotificationSetting']);
