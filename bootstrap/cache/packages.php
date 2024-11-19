28c9fb6ab8d292638dced1caff35b91fa509a034
Muhammad Fayyaz <fayyazjee11@gmail.com>
2024-11-19 15:18:43 +0500

    string length for wamp

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -52,6 +52,17 @@
       0 => 'Laravel\\Ui\\UiServiceProvider',
     ),
   ),
+  'maatwebsite/excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
+  ),
   'mailersend/laravel-driver' => 
   array (
     'providers' => 
