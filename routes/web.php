7b8f0086a828899d46ce0ccd035f17e7124846a4
Muhammad Fayyaz <fayyazjee11@gmail.com>
2024-11-29 18:24:21 +0500

    pop up modal db table and modal done

--- routes/web.php
+++ routes/web.php
@@ -101,6 +101,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('general-setting', [ApiGeneralController::class, 'generalSetting'])->name('generalSetting');
     Route::post('maintenance', [ApiGeneralController::class, 'maintenance'])->name('maintenance');
 
+    Route::get('rating-popup', [ApiGeneralController::class, 'trialPeriod'])->name('trial-period');
+
     Route::resource('coupon', CouponController::class);
     Route::get('get-coupon/{id}', [CouponController::class, 'getCoupon']);
 
