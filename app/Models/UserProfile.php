beaad0943eb1fd4305827c04648db06e02c30f9a
danyal hassan <danyalhassan651@gmail.com>
2024-12-19 17:45:00 +0500

    set

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -11,8 +11,21 @@ class UserProfile extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded=[];
+
+    protected $appends = [
+        'max_post_mbs'
+    ];
     public function Following()
     {
         return $this->hasOne(FollowerList::class, 'follower_id', 'user_id')->where('user_id', auth()->id());
+    }
+
+    public function getMaxPostMbsAttribute(){
+        $data = SubscriptionPlan::where('id', auth()->user()->subscription_id)->first();
+        if($data){
+            return "$data->image_size_limit";
+        }else{
+            return "0";
+        }
     }
 }
