80b176a902863cbcb93f0e8e96e81c79be9591e5
danyal hassan <danyalhassan651@gmail.com>
2025-01-01 19:29:36 +0500

    set

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -60,7 +60,10 @@ class AuthController extends Controller
                 $auth = null;
             }
         } else {
-            if ($request->user_account_id) {
+            $auth = Auth::attempt(['email' => $request->email, 'password' => $request->password]);
+        }
+        if ($auth) {
+            if (isset($request->user_account_id) && $request->user_account_id != null) {
                 $useraccount = User::where('id', $request->user_account_id)->first();
                 if ($useraccount && $useraccount->user_account_id != null) {
                     $user_account_id = $useraccount->user_account_id;
@@ -70,9 +73,6 @@ class AuthController extends Controller
             } else {
                 $user_account_id = null;
             }
-            $auth = Auth::attempt(['email' => $request->email, 'password' => $request->password]);
-        }
-        if ($auth) {
             if($request->current_user_id){
                 User::where('id', $request->current_user_id)->update([
                     'fcm_token' => null
