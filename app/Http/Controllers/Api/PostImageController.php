a93c4bbf1a034fed99ba87f007af9e9fd99c84c5
danyal hassan <danyalhassan651@gmail.com>
2025-01-09 17:28:06 +0500

    set

--- app/Http/Controllers/Api/PostImageController.php
+++ app/Http/Controllers/Api/PostImageController.php
@@ -924,50 +924,66 @@ class PostImageController extends Controller
         $filesOriginalArray = $post->original_image;
         $baseUrl = 'https://picastro.beckapps.co/public/';
 
-        // Mark deleted images in $filesArray and prepare for new images
+        $newImagesIndex = 0; // Index to track new images
+
+        // Replace deleted images in $filesArray
         foreach ($deletedImagesArray as $deletedImagePath) {
             Log::info('Deleting Image:', [$deletedImagePath, $filesArray]);
             if (($key = array_search($deletedImagePath, $filesArray)) !== false) {
-                $filesArray[$key] = null; // Mark as deleted
+                // Replace with new image if available
+                if (isset($newImages[$newImagesIndex])) {
+                    $filesArray[$key] = str_replace($baseUrl, '', $newImages[$newImagesIndex]);
+                    $newImagesIndex++;
+                }
             }
         }
 
-        // Replace deleted images with new images
-        foreach ($newImages as $index => $newImage) {
-            if (($key = array_search(null, $filesArray)) !== false) {
-                $filesArray[$key] = str_replace($baseUrl, '', $newImage); // Add new image at deleted index
-            } else {
-                $filesArray[] = str_replace($baseUrl, '', $newImage); // Add new image if no slots are free
-            }
+        // Append any remaining new images to the end of $filesArray
+        while ($newImagesIndex < count($newImages)) {
+            $filesArray[] = str_replace($baseUrl, '', $newImages[$newImagesIndex]);
+            $newImagesIndex++;
         }
 
-        // Process deleted original images
+        $newOriginalImagesIndex = 0; // Index to track new original images
+
+        // Replace deleted original images in $filesOriginalArray
         foreach ($deletedOriginalImagesArray as $deletedOriginalImagePath) {
             if (($key = array_search($deletedOriginalImagePath, $filesOriginalArray)) !== false) {
-                $filesOriginalArray[$key] = null; // Mark as deleted
+                // Replace with new original image if available
+                if (isset($newOriginalImages[$newOriginalImagesIndex])) {
+                    $filesOriginalArray[$key] = $newOriginalImages[$newOriginalImagesIndex];
+                    $newOriginalImagesIndex++;
+                }
             }
         }
 
-        // Replace deleted original images with new original images
-        foreach ($newOriginalImages as $index => $newOriginalImage) {
-            if (($key = array_search(null, $filesOriginalArray)) !== false) {
-                $filesOriginalArray[$key] = $newOriginalImage; // Add new original image at deleted index
-            } else {
-                $filesOriginalArray[] = $newOriginalImage; // Add new original image if no slots are free
-            }
+        // Append any remaining new original images to the end of $filesOriginalArray
+        while ($newOriginalImagesIndex < count($newOriginalImages)) {
+            $filesOriginalArray[] = $newOriginalImages[$newOriginalImagesIndex];
+            $newOriginalImagesIndex++;
         }
 
-
+        // Handle uploaded image
         if ($request->hasFile('image')) {
-            $imageName         =  $this->imageUpload($request->file('image'), 'assets/uploads/postimage/', true);
-            $originalImageName =  $this->originalImageUpload($request->file('image'), 'images/', false, true);
+            $imageName = $this->imageUpload($request->file('image'), 'assets/uploads/postimage/', true);
+            $originalImageName = $this->originalImageUpload($request->file('image'), 'images/', false, true);
 
-            $updatedFilesArray = array_merge($filesArray, $imageName);
-            $updatedOriginalFilesArray = array_merge($filesOriginalArray, $originalImageName);
-        } else {
-            $updatedFilesArray = $filesArray;
-            $updatedOriginalFilesArray = $filesOriginalArray;
+            // Append uploaded files to their respective arrays
+            if (is_array($imageName)) {
+                $filesArray = array_merge($filesArray, $imageName);
+            } else {
+                $filesArray[] = $imageName;
+            }
+
+            if (is_array($originalImageName)) {
+                $filesOriginalArray = array_merge($filesOriginalArray, $originalImageName);
+            } else {
+                $filesOriginalArray[] = $originalImageName;
+            }
         }
+
+        $updatedFilesArray = $filesArray;
+        $updatedOriginalFilesArray = $filesOriginalArray;
 
         $tableName = 'post_images';
         $uniqueId = $id; // Replace with the actual unique ID or value
