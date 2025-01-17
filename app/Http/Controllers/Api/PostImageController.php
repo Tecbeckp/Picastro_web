e762785a281d8fdc50e90884e616b4fa56b0443e
danyal hassan <danyalhassan651@gmail.com>
2025-01-17 15:28:04 +0500

    set

--- app/Http/Controllers/Api/PostImageController.php
+++ app/Http/Controllers/Api/PostImageController.php
@@ -270,7 +270,7 @@ class PostImageController extends Controller
 
             // Paginate the result
             $currentPage = request()->get('page', 1); // Get current page or default to 1
-            $perPage = 10;
+            $perPage = 50;
 
             // Slice and paginate
             $paginatedPosts = new LengthAwarePaginator(
