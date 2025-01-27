ae44202a1b4ba1ef91f8e8448d2f907edb5b66da
danyal hassan <danyalhassan651@gmail.com>
2025-01-27 13:22:16 +0500

    set disable source view page

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -135,12 +135,5 @@
                 }
             });
         }
-        document.addEventListener('contextmenu', event => event.preventDefault());
-        document.addEventListener('keydown', function(event) {
-            if (event.ctrlKey && event.key === 'u') {
-                event.preventDefault();
-                console.log("Viewing source is disabled!");
-            }
-        });
     </script>
 @endpush
