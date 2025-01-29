8b5528455d1c03e0867bc349724bfd969057d116
danyal hassan <danyalhassan651@gmail.com>
2025-01-29 19:06:55 +0500

    set

--- app/Http/Controllers/Api/ApiGeneralController.php
+++ app/Http/Controllers/Api/ApiGeneralController.php
@@ -1132,12 +1132,12 @@ class ApiGeneralController extends Controller
             'user_type' => 'required'
         ]);
 
-        // if ($request->user_type == 'All') {
-        //     $users = User::whereNot('id', '1')->get();
-        // } else {
-        //     $users = User::whereNot('id', '1')->where('subscription', $request->user_type)->get();
-        // }
-        $users = User::whereIn('id', ['43','38','58'])->get();
+        if ($request->user_type == 'All') {
+            $users = User::whereNot('id', '1')->get();
+        } else {
+            $users = User::whereNot('id', '1')->where('subscription', $request->user_type)->get();
+        }
+        // $users = User::whereIn('id', ['43','38','58'])->get();
 
         if ($users) {
             $success_user = [];
